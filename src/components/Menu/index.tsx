37f1f871e5f39a439f313f1bf51b78dcdf88a116
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2024-05-08 11:50:53 -0300

    up code

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -55,15 +55,15 @@ export function Hamb({ onClick, clickHamb }: any) {
 
     return (
         <div className={`hamb ${clickHamb ? 'hambAtivo' : ''}`} onClick={onClick}>
-            {clickHamb ? <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+            {clickHamb ? <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#999999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
                 :
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                     <g id="SVGRepo_iconCarrier">
-                        <path d="M4 18L20 18" stroke="#333" strokeWidth="2" strokeLinecap="round"></path>
-                        <path d="M4 12L20 12" stroke="#333" strokeWidth="2" strokeLinecap="round"></path>
-                        <path d="M4 6L20 6" stroke="#333" strokeWidth="2" strokeLinecap="round"></path>
+                        <path d="M4 18L20 18" stroke="#999999" strokeWidth="2" strokeLinecap="round"></path>
+                        <path d="M4 12L20 12" stroke="#999999" strokeWidth="2" strokeLinecap="round"></path>
+                        <path d="M4 6L20 6" stroke="#999999" strokeWidth="2" strokeLinecap="round"></path>
                     </g>
                 </svg>
             }
